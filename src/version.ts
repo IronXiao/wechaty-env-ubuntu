2ef52118f0284f428ebb1f0dd7f108dcae0355f1
Iron Xiao <xiaoleigs@gmail.com>
2023-03-14 01:43:30 +0000

    init

---
+++ src/version.ts
@@ -0,0 +1,5 @@
+/**
+ * This file was auto generated from scripts/generate-version.sh
+ */
+export const VERSION: string = '0.65.15'
+export const GIT_COMMIT_HASH: string = '5ad6690951f59a6ed067ede40a967e7fd6cfb848'
